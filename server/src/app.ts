8fd5800415828f4ca9533746d21e07264166c3d4
varrb <varrbor@ukr.net>
2021-03-09 22:06:00 +0200

    fixed config

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,24 +3,23 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import todoRoutes from './routes/todos.routes';
 import authRoutes from './routes/auth.routes';
+import config from 'config';
 
 const app: Express = express();
 const bodyParser = require('body-parser');
 
-const PORT: string | number = process.env.PORT || 4000;
-
+const PORT: string | number = config.get('port') || 4000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(todoRoutes);
 app.use('/api/auth', authRoutes);
 
-const uri: string = `mongodb+srv://root:y60s73A71toUpKrk@devconnector.mufnt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose.set('useFindAndModify', false);
 
 mongoose
-	.connect(uri, options)
+	.connect(config.get('mongoUri'), options)
 	.then(() =>
 		app.listen(PORT, () =>
 			console.log(`Server running on http://localhost:${PORT}`)
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -21,36 +21,3 @@ const UserSchema: Schema = new Schema(
 
 export default model<IUser>('User', UserSchema);
 
-// import mongoose from 'mongoose'
-// import bcrypt from 'bcrypt'
-// import isEmail from 'validator/lib/isEmail.js'
-//
-// const userSchema = mongoose.Schema({
-//     name: {
-//       type: String,
-//       required: [true, 'Please enter a name'],
-//     },
-//     email: {
-//       type: String,
-//       required: [true, 'Please enter an email'],
-//       unique: true,
-//       validate: [isEmail, 'Please enter a valid email']
-//     },
-//     password: {
-//       type: String,
-//       required: [true, 'Please enter a password'],
-//       minlength: [6, 'Minimum password length is 6 characters'],
-//     },
-//     accessToken: String,
-// })
-//
-//
-// userSchema.pre('save', async function(next) {
-//     const salt = await bcrypt.genSalt();
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-//   });
-//
-//   const User = mongoose.model('user',userSchema)
-//
-// export default User
